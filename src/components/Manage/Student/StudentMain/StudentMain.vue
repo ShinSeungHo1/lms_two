79f52db1347b4add9e12081a8e5f43d079f9f68d
ShinSeungHo1 <ssh941424@gmail.com>
2025-07-28 12:32:49 +0900

    feat.사용자관리/학생관리 완료 (#4)

--- src/components/Manage/Student/StudentMain/StudentMain.vue
+++ src/components/Manage/Student/StudentMain/StudentMain.vue
@@ -1,91 +1,149 @@
 <script setup>
-import PageNavigation from '@/components/common/PageNavigation.vue';
 import axios from 'axios';
+import { ElMessage, ElMessageBox } from 'element-plus';
 import { onMounted, ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { useModalState } from '@/stores/modalState';
+
+import PageNavigation from '@/components/common/PageNavigation.vue';
 import StudentModal from '../StudentModal/StudentModal.vue';
 
+const headerList = ['학번', '이름', '연락처', '가입일자', '취업여부', '재학상태'];
+
 const route = useRoute();
-const studentList = ref([]);
-const studentCount = ref(0);
 const modalState = useModalState();
-const detailId = ref(0);
 
-const studentSearch = (cPage = 1) => {
-  const param = new URLSearchParams(route.query);
-  console.log(route.query);
-  param.append('currentPage', cPage);
-  param.append('pageSize', 5);
+const studentList = ref([]);
+const listCount = ref(0);
 
-  axios.post('/api/manage/studentListBody.do', param).then((res) => {
-    studentList.value = res.data.list;
-    studentCount.value = res.data.count;
-  });
-}
+const studentId = ref('');
 
-onMounted(() => {
-  studentSearch();
-});
+const searchStudentList = (cPage = 1) => {
+  const param = {
+    currentPage: cPage,
+    pageSize: 5,
+    ...route.query,
+  };
 
-const studentDetail = (id) => {
-  modalState.$patch({ isOpen: true })
-  detailId.value = id;
-}
+  const urlParam = new URLSearchParams(param);
+
+  axios
+    .post('/api/manage/studentListBody.do', urlParam)
+    .then((res) => {
+      console.log(res);
+      console.log('학생 리스트 불러오기 성공!');
+      studentList.value = res.data.list;
+      listCount.value = res.data.count;
+    })
+    .catch((err) => {
+      console.log('학생 리스트 불러오기 실패 err: ', err);
+    });
+};
+
+const changeStatus = (id, prevStatus, changedStatus) => {
+  ElMessageBox.confirm('재학 상태를 변경하시겠습니까?', {
+    confirmButtonText: '변경',
+    cancelButtonText: '취소',
+    type: 'warning',
+  })
+    .then(() => {
+      const param = {
+        studentId: id,
+        studentStatus: changedStatus,
+      };
+
+      axios
+        .post('/api/manage/student-status', param)
+        .then((res) => {
+          if (res.data === 'SUCCESS') {
+            ElMessage.success('재학 상태가 변경되었습니다.');
+            searchStudentList();
+          } else {
+            ElMessage.error('변경에 실패하였습니다.');
+          }
+        })
+        .catch((err) => {
+          ElMessage.error('서버 오류 발생 err : ', err);
+        });
+    })
+    .catch(() => {
+      ElMessage.info('재학 상태 변경 취소');
+      searchStudentList();
+    });
+};
+
+const studentDetailModal = (id) => {
+  modalState.$patch({ isOpen: true, type: 'studentDetail' });
+  studentId.value = id;
+};
 
 watch(
   () => route.query,
   () => {
-    studentSearch()
+    searchStudentList();
   },
-)
+);
 
+onMounted(() => {
+  searchStudentList();
+});
 </script>
 
 <template>
-  <div class="student-main-container">
+  <div class="student-table-container">
     <table class="student-table">
       <thead class="student-table-header">
-        <tr>
-          <th>학번</th>
-          <th>이름</th>
-          <th>연락처</th>
-          <th>가입일자</th>
-          <th>취업여부</th>
-          <th>재학상태</th>
+        <tr class="student-table-header-row">
+          <th
+            v-for="(header, index) in headerList"
+            :key="`h-${index}`"
+            class="student-table-header-cell"
+          >
+            {{ header }}
+          </th>
         </tr>
       </thead>
-      <tbody>
-        <template v-if="studentCount > 0">
-          <tr v-for="student in studentList" :key="student.studentId" class="student-table-row">
-            <td class="student-cell">{{ student.studentNumber }}</td>
-            <td class="student-cell cursor-pointer hover:underline" @click="studentDetail(student.studentId)">{{
-              student.studentName }}</td>
-            <td class="student-cell">{{ student.studentHp }}</td>
-            <td class="student-cell">{{ student.studentRegDate.substr(0, 10) }}</td>
-            <td class="student-cell">
-              <span>{{ student.studentEmpStatus === 'Y' ? '취업' : '미취업' }}</span>
-            </td>
-            <td class="student-cell">
-              <select v-model="student.statusYN">
-                <option value="W">승인 대기중</option>
-                <option value="Y">재학</option>
-                <option value="N">탈퇴</option>
-              </select>
-            </td>
-          </tr>
-        </template>
-        <template v-else>
-          <tr>
-            <td colspan="6" class="student-empty-row">일치하는 학생이 없습니다.</td>
-          </tr>
-        </template>
+      <tbody class="student-table-body">
+        <tr v-for="student in studentList" :key="student.studentId" class="student-table-body-row">
+          <td class="student-table-body-cell">
+            {{ student.studentNumber }}
+          </td>
+          <td class="student-table-body-cell" @click="studentDetailModal(student.studentId)">
+            {{ student.studentName }}
+          </td>
+          <td class="student-table-body-cell">
+            {{ student.studentHp }}
+          </td>
+          <td class="student-table-body-cell">
+            {{ student.studentRegDate?.substring(0, 10) }}
+          </td>
+          <td class="student-table-body-cell">
+            {{ student.studentEmpStatus === 'Y' ? '취업' : '미취업' }}
+          </td>
+          <td class="student-table-body-cell">
+            <select
+              id="student-change-status"
+              :value="student.statusYN"
+              @change="changeStatus(student.studentId, student.statusYN, $event.target.value)"
+            >
+              <option value="Y">재학</option>
+              <option value="N">탈퇴</option>
+              <option value="W">승인 대기중</option>
+            </select>
+          </td>
+        </tr>
       </tbody>
     </table>
-    <PageNavigation :total-items="studentCount" :items-per-page="5" :on-page-change="studentSearch" />
   </div>
-  <StudentModal v-if="modalState.isOpen" :detail-id="detailId" @post-success="studentSearch()"
-    @un-mounted-modal="detailId = $event" />
+  <PageNavigation
+    :total-items="listCount"
+    :items-per-page="5"
+    :on-page-change="searchStudentList"
+  />
+  <StudentModal
+    v-if="modalState.type === 'studentDetail' && modalState.isOpen"
+    :student-id="studentId"
+  />
 </template>
 
 <style>
--- src/views/Manage/Student.vue
+++ src/views/Manage/Student.vue
@@ -1,11 +1,11 @@
 <script setup>
 import ContentBox from '@/components/common/ContentBox.vue';
-import StudentMain from '@/components/Manage/Student/StudentMain/StudentMain.vue';
 import StudentSearch from '@/components/Manage/Student/StudentSearch/StudentSearch.vue';
+import StudentMain from '@/components/Manage/Student/StudentMain/StudentMain.vue';
 </script>
 
 <template>
-  <ContentBox> 학생 관리</ContentBox>
+  <ContentBox>학생 관리</ContentBox>
   <StudentSearch />
   <StudentMain />
 </template>
